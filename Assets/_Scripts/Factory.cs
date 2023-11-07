4544f6220ad9b8ff42cbaf37b5215634c7c8f151
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2023-11-07 20:21:36 +0300

    1

--- Assets/_Scripts/Factory.cs
+++ Assets/_Scripts/Factory.cs
@@ -2,35 +2,32 @@ using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Factory : MonoBehaviour 
+public class Factory : MonoBehaviour
 {
     [SerializeField] private GameObject _chunkPrefab;
     [SerializeField] private GameObject[] _obstaclePrefabs;
-    [SerializeField][Range(0.0f, 1.0f)] private float _obstacleSpawnChance;
-
-
+    [SerializeField][Range(0.0f, 1.0f)] private float _obstacleSpawnChance;    
     public void SpawnChunk(Transform spawnChankPosition)
     {
         GameObject chunkContainer = new GameObject("ChunkContainer");
         GameObject chunkObject = Instantiate(_chunkPrefab,
-            spawnChankPosition.position + spawnChankPosition.forward * 10f,   
+            spawnChankPosition.position + spawnChankPosition.forward * 10f,
             spawnChankPosition.rotation, chunkContainer.transform);
 
         Chunk chunk = chunkObject.GetComponent<Chunk>();
 
         if (chunk == null) return;
 
-        foreach (Transform spawnObstaclePosition in chunk._obstacleSpawnPositions)                // генерация преград на чанке
+        foreach (Transform spawnObstaclePosition in chunk._obstacleSpawnPositions)
         {
             if (Random.value < _obstacleSpawnChance)
             {
                 int randomObstacleIndex = Random.Range(0, _obstaclePrefabs.Length);
-                Instantiate(_obstaclePrefabs[randomObstacleIndex], 
-                             spawnObstaclePosition.position, 
-                             Quaternion.Euler(0f, Random.Range(0f, 360f), 0f), 
+                Instantiate(_obstaclePrefabs[randomObstacleIndex],
+                             spawnObstaclePosition.position,
+                             Quaternion.Euler(0f, Random.Range(0f, 360f), 0f),
                              chunkContainer.transform);
             }
-        }
+        }      
     }
 }
-
